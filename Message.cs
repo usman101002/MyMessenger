c2804d10bc42d0747d476d4e75f161d91d12a4c8
Усман <@DESKTOP-E07M5P8>
2023-12-11 00:55:21 +0700

    Add serialization

--- Message.cs
+++ Message.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace MyMessenger
 {
+    [Serializable]
     public class Message
     {
         public string UserName {get; set; }
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace MyMessenger
 {
@@ -10,8 +11,15 @@ namespace MyMessenger
     {
         static void Main(string[] args)
         {
-            Message msg = new Message();
-            Console.WriteLine(msg.ToString());
+        //    Message msg = new Message();
+        //    Console.WriteLine(msg.ToString());
+
+        var msg = new Message("Usman", "Privet", DateTime.Now);
+        var output = JsonConvert.SerializeObject(msg);
+        Console.WriteLine(output);
+
+        var deserializedMessage = JsonConvert.DeserializeObject<Message>(output);
+        Console.WriteLine(deserializedMessage);
         }
     }
 }
